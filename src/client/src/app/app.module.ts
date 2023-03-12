44f11f4be2e77b353fcd8a3cf7312c663458e89e
Jens Schmitz <jensschmitz1@hotmail.com>
2023-03-12 02:10:45 +0100

    create first structure - time for bed X﹏X

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { API_BASE_URL } from '../../.api/nswag';
 import { environment } from '../../environment';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleListComponent } from './components/article-list/article-list.component';
 
@@ -16,7 +15,6 @@ import { ArticleListComponent } from './components/article-list/article-list.com
   ],
   imports: [
     BrowserModule, 
-    AppRoutingModule, 
     HttpClientModule
   ],
   providers: [
--- src/client/src/app/components/article-list/article-list.component.ts
+++ src/client/src/app/components/article-list/article-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AggregatedArticle } from '../../../../.api/nswag';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-article-list',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-list.component.scss']
 })
 export class ArticleListComponent {
+  public _articleCount$: BehaviorSubject<number> = new BehaviorSubject(0);
+  public _articles$: BehaviorSubject<AggregatedArticle[]> = new BehaviorSubject([new AggregatedArticle()]);
 
+  public get articleCount$(): Observable<number> {
+    return this._articleCount$.asObservable();
+  }
+
+  public get articles$(): Observable<AggregatedArticle[]> {
+    return this._articles$.asObservable();
+  }
+
+  constructor(private _articleService: ArticleService) {}
+
+  public getArticleCount(): void {
+    this._articleService.getArticleCount().subscribe((count) => {
+      console.log(count);
+      this._articleCount$.next(count);
+    });
+  }
+
+  public getArticles(): void {
+    this._articleService.getArticles().subscribe((articles) => {
+      console.log(articles);
+      this._articles$.next(articles);
+    });
+  }
 }
